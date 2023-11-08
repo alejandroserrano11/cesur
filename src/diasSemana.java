f48ef6cafc25e8cf84b1712d36bdf728aa5fc25a
alejandroserrano11 <alejandro.s383963@cesurformacion.com>
2023-11-08 12:23:18 +0100

    funciona

---
+++ src/diasSemana.java
@@ -0,0 +1,79 @@
+
+public class diasSemana {
+	public static void main(String[] args) {
+
+		int diaSemana = 0;
+		int semana = 0;
+		int mesaño = 0;
+		while (mesaño < 12) {
+			switch (mesaño) {
+			case 0:
+				System.out.print("Enero ");
+				break;
+			case 1:
+				System.out.print("Febrero ");
+				break;
+			case 2:
+				System.out.print("Marzo ");
+				break;
+			case 3:
+				System.out.print("Abril ");
+				break;
+			case 4:
+				System.out.print("Mayo ");
+				break;
+			case 5:
+				System.out.print("Junio ");
+				break;
+			case 6:
+				System.out.print("Julio ");
+				break;
+			case 7:
+				System.out.print("Agosto ");
+				break;
+			case 8:
+				System.out.print("Septiembre ");
+				break;
+			case 9:
+				System.out.print("Octubre ");
+				break;
+			case 10:
+				System.out.print("Noviembre ");
+				break;
+			case 11:
+				System.out.print("Diciembre ");
+				break;
+
+			}
+			semana = 0;
+			while (semana < 4) {
+				diaSemana = 0;
+				while (diaSemana <= 7) {
+					if (diaSemana == 1) {
+						System.out.print("[L" + " ");
+					} else if (diaSemana == 2) {
+						System.out.print("M" + " ");
+					} else if (diaSemana == 3) {
+						System.out.print("X" + " ");
+					} else if (diaSemana == 4) {
+						System.out.print("J" + " ");
+					} else if (diaSemana == 5) {
+						System.out.print("V" + " ");
+					} else if (diaSemana == 6) {
+						System.out.print("S" + " ");
+					} else if (diaSemana == 7) {
+						System.out.print("D]" + " ");
+					}
+
+					diaSemana = diaSemana + 1;
+
+				}
+				semana = semana + 1;
+			}
+			System.out.println();
+
+			mesaño = mesaño + 1;
+		}
+
+	}
+}
