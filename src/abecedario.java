93736861c11ecfaa42956d375e25dcb0fd7929e9
alejandroserrano11 <alejandro.s383963@cesurformacion.com>
2023-11-08 10:04:28 +0100

    abecedario

---
+++ src/abecedario.java
@@ -0,0 +1,31 @@
+
+
+public class abecedario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		for (int cont=97; cont<97+27-1; cont++) {
+			System.out.print((char) cont+ " ");
+		}
+		
+	 System.out.println();
+	 
+	 
+		int cont = 97;
+				
+				
+		while (cont < 97+27-1 ) {
+			System.out.print((char) cont + " ");
+			cont = cont + 1;
+			
+			
+			
+			
+			
+		}
+	}
+
+}
