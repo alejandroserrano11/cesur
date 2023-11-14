82e3c52fc136edc29d4de8bd91384b7927f6d073
alejandroserrano11 <alejandro.s383963@cesurformacion.com>
2023-11-14 12:51:42 +0100

    añadido maximo minimo y salto

--- src/contador_inverso.java
+++ src/contador_inverso.java
@@ -1,8 +1,15 @@
 //vamos a ir de 1200 a 620 contando de 20 en 20 usando while, do while y for
 
+import java.util.Scanner;
+
 public class contador_inverso {
 	public static void main(String[] args) {
 
+		Scanner teclado = new Scanner(System.in);
+
+		int maximo = 0;
+		int minimo = 0;
+		int salto = 0;
 		int contador = 1200;
 
 		while ((contador >= 620)) {
@@ -26,6 +33,31 @@ public class contador_inverso {
 		for (int i = 1200; i >= 620; i = i - 20) {
 			System.out.print(i + " ");
 
+		}
+		System.out.println();
+
+		System.out.print("Introduce el número máximo: ");
+
+		maximo = teclado.nextInt();
+
+		System.out.println();
+
+		System.out.print("Introduce el número mínimo: ");
+		minimo = teclado.nextInt();
+		while (minimo > maximo) {
+			System.out.print("Introduce el número mínimo: ");
+			minimo = teclado.nextInt();
+		}
+		System.out.println();
+
+		System.out.print("Introduce el salto: ");
+
+		salto = teclado.nextInt();
+
+		System.out.println();
+		
+		for (int x = maximo; x >= minimo; x = x - salto) {
+			System.out.print(x + " ");
 		}
 
 	}
