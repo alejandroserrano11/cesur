50816e4019474d00ac7d476202f75db52d03e7dd
alejandroserrano11 <alejandro.s383963@cesurformacion.com>
2023-11-27 12:00:05 +0100

    hola mundo metodos

---
+++ src/hola_mundo.java
@@ -0,0 +1,27 @@
+
+public class hola_mundo {
+
+	public static void main(String[] args) {
+
+		String cadena = " hola mundo ";
+
+		for (int indice = 0; indice < cadena.length(); indice++) {
+			if ((indice % 2) == 0) {
+				imprime(cadena.toUpperCase(), indice);
+			} else {
+				imprime(cadena, indice);
+
+			}
+		}
+		System.out.println(cadena);
+
+		cadena = cadena.toUpperCase();
+		System.out.println(cadena);
+
+	}
+
+	static void imprime(String cadena, int indice) {
+		System.out.println(cadena.charAt(indice));
+	}
+
+}
