e245a57362b7b06113fd618b24dc0e52db27efcc
alejandroserrano11 <alejandro.s383963@cesurformacion.com>
2023-12-04 13:26:03 +0100

    getter setter cadena array

---
+++ src/loko.java
@@ -0,0 +1,72 @@
+
+public class loko {
+
+	public static void main(String[] args) {
+		String[] cadenaInicial = new String[10];
+		for (int i=0; i<3; i++) {
+			cadenaInicial[i] = "hola";
+		}
+		ListaFijaCadena cadena1;
+		cadena1 = new ListaFijaCadena(3,cadenaInicial);
+		System.out.println("num elementos: " + cadena1.getNumElementos());
+		for (int i = 0; i<cadena1.getNumElementos(); i++) {
+			System.out.println(cadena1.getArray()[i]);
+		}
+		// Crear ListaFijaCadena
+		// metemos 3 elementos
+		// sacamos por pantalla el total de elementos y los elementos.
+
+	} // end of main
+
+} // end of loko
+
+class ListaFijaCadena { // ahora va ser inmutable.
+	// atributos:
+
+	// numero de elementos
+	private int numElementos = 0;
+
+	// array de Strings
+	private String[] array = new String[MAXNUM];
+
+	// MAXNUM de elementos
+	private static final int MAXNUM = 10;
+	
+	
+	
+	// metodos:
+	
+	// setter y getter de cada atributo
+	
+	public int getNumElementos() {
+		return numElementos;
+	}
+
+	public void setNumElementos(int numElementos) {
+		this.numElementos = numElementos;
+	}
+
+	public String[] getArray() {
+		return array;
+	}
+
+	public void setArray(String[] array) {
+		this.array = array;
+	}
+
+	public static int getMaxnum() {
+		return MAXNUM;
+	}
+
+	public ListaFijaCadena(int numElementos, String[] array) {
+		this.numElementos = numElementos;
+		this.array = array;
+	}
+
+	
+	
+	// constructor con parametros de todos sus atributos
+
+
+	
+}
